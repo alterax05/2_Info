93c1e8df5c612e314ad448306c89976709385507
alterax04 <miky.porcellato@gmail.com>
2022-09-16 17:46:32 +0200

    Aggiungere i file di progetto.

---
+++ Contenitore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Info
+{
+    internal class Contenitore
+    {
+        private int[] array;
+        private int dim;
+
+        public Contenitore()
+        {
+            array = new int[100];
+            dim = 0;
+        }
+
+        //public void Inserisci()
+        //{
+        //    for (int i = 0; i < dim; i++)
+        //    {
+        //        Console.Write("Inserisci il numero: ");
+        //        array[i] = Convert.ToInt32(Console.ReadLine());
+        //    }
+        //}
+
+        public void InserimentoOrdinato(int n)
+        {
+            array[dim] = n;  //metto il nuovo valore alla fine
+            int i = dim;
+            bool finito = false;
+
+            while (i > 0 && !finito)
+            {
+                if (array[dim] < array[i - 1])
+                {
+                    array[i] = array[i - 1];
+                    array[i - 1] = n;
+                    i--;
+                }
+                else
+                {
+                    finito = true;
+                }
+            }
+            dim++;
+        }
+        public void Print()
+        {
+            foreach (int i in array)
+            {
+                if (i!=0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _2_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci dim: ");
+            int dim = Convert.ToInt32(Console.ReadLine());
+
+            Contenitore v = new Contenitore();
+            //v.Inserisci();
+            for (int i = 0; i < dim; i++)
+            {
+                Console.Write("Inserisci numero: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                v.InserimentoOrdinato(num);
+            }
+            v.Print();
+        }
+    }
+}
